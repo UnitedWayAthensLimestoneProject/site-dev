5ea92a06ecb5b5f1ae0c6fa8764ea20eeae31fe9
nhershy <nhershy@users.noreply.github.com>
2017-09-06 13:34:48 -0500

    Upload sidebar imgs with random digits
    4 random digits appended to each filename of the sidebar images when uploaded. This is to force reload cache so uploaded images update browser cache so changes are reflected on page.

--- sidebarUpload.php
+++ sidebarUpload.php
@@ -3,29 +3,36 @@
 $filename = $_FILES['file']['name'];
 $tmp_name = $_FILES['file']['tmp_name'];
 $defaultName = $_POST['sBar'];
-$fileExtBefore = (explode('.', $filename));
-$fileExt = strtolower(end($fileExtBefore));
-$allowedExt = array('jpg', 'jpeg', 'png', 'gif');
-$newName = $defaultName . '.' . $fileExt;
+$dir = "sidebarImgs/";
+$filesAry = scandir($dir); //used to delete old file
+$randNum = mt_rand(1000,9999); //generates 4 digit random number
+$randNumString = strval($randNum); //4 digit random string
 
-if (in_array($fileExt, $allowedExt))
-{
-  if (isset($filename)) {
-    if (!empty($filename)) {
+if (isset($filename)){
+  if (!empty($filename)) {
+    $fileExtBefore = (explode('.', $filename));
+    $fileExt = strtolower(end($fileExtBefore));
+    $allowedExt = array('jpg', 'jpeg', 'png', 'gif');
+
+    if (in_array($fileExt, $allowedExt))
+    {
       //define ('SITE_ROOT', realpath(dirname(__FILE__)));
+      $randName = $defaultName . $randNumString; // e.g. sidebar1####
+      $newName = $randName . '.' . $fileExt;
+
       $location = 'sidebarImgs/';
       if(move_uploaded_file($tmp_name, $location.$newName)) {
         echo 'File uploaded successfully';
       }
-    }
-    else {
-      echo 'Please choose a file';
+    } else {
+      echo 'File type not supported';
     }
   }
+} else {
+  echo 'Please choose a file';
 }
-else {
-  echo 'File type not supported';
-}
+
+
 
 ?>
 
