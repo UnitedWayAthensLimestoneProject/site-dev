b258b025e79eeca15e3bacf1ddf7cf4b2ed142b9
nhershy <nhershy@users.noreply.github.com>
2017-10-07 16:59:01 -0500

    Removed 'mysqli' cmnds from database_connection.php

--- admin/scripts/database_connection.php
+++ admin/scripts/database_connection.php
@@ -1,24 +1,10 @@
 <?php
-
 	require_once 'app_config.php';
 
 	mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD)
 		or handle_error("an error occurred while attempting to connect to the database", mysql_error());
 
 	mysql_select_db(DATABASE_NAME)
-		or handle_error("Error selecting the database ", mysqli_error());
-
-
-
-	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-
-	//Test Connection
-	if (mysqli_connect_errno())
-	{
-		die ("Database connection failed: " .
-			mysqli_connect_error() .
-			 "(" . mysqli_connect_errno() . ")"
-			);
-	}
+		or handle_error("Error selecting the database ", mysql_error());
 
 ?>
