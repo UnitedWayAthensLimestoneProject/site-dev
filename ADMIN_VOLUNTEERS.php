34f6537077eb964bc59dcfdd0b22b9a5fa04c5a3
nhershy <nhershy@users.noreply.github.com>
2017-10-07 22:26:47 -0500

    Select volunteers and display list of email addresses
    In an HTML table are listed all the volunteers, ordered by last name. Each volunteer can be selected using their associated checkboxes. After desired names have been selected, scroll to the bottom of the table and choose the "Submit" button. Page will refresh and a list of email addresses separated by commas will be displayed.

--- ADMIN_VOLUNTEERS.php
+++ ADMIN_VOLUNTEERS.php
@@ -1,5 +1,17 @@
 <?php
 
+function printStuff($array) {
+	foreach( $array as $stuff ) {
+	    if( is_array( $stuff ) ) {
+	        foreach( $stuff as $thing ) {
+	            echo $thing . ", ";
+	        }
+	    } else {
+	        echo $stuff;
+	    }
+	}
+}
+
 require_once 'admin/scripts/app_config.php';
 
 $conn = mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD)
@@ -10,16 +22,28 @@ mysql_select_db(DATABASE_NAME)
 
 	//--------------------------------------
 
+echo "Results: <br>";
+printStuff($_POST);
+echo "<br> ";
+
 $query = "SELECT * FROM volunteers ORDER BY last_name ASC";
 $result = mysql_query($query);
 
+echo "<form action='' method='post'>";
 echo "<table border='1'>";
-echo "<tr><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Home Phone</td><td>Cell Phone</td><tr>";
+echo "<tr><td></td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Home Phone</td><td>Cell Phone</td><tr>";
 while ($row = mysql_fetch_assoc($result)) {
-    //echo $row["agy_city"] . "<br>";
-		echo "<tr><td>{$row['first_name']}</td><td>{$row['last_name']}</td><td>{$row['email_address']}</td><td>{$row['home_phone']}</td><td>{$row['cell_phone']}</td><tr>";
+	echo "<tr><td><input type='checkbox' name='selected[]' value='{$row['email_address']}'/></td><td>{$row['first_name']}</td><td>{$row['last_name']}</td><td>{$row['email_address']}</td><td>{$row['home_phone']}</td><td>{$row['cell_phone']}</td><tr>";
 }
 echo "</table>";
+echo "<input type='submit'/>";
+echo "</form>";
+
+/*
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+*/
 
 mysql_free_result($result);
 
