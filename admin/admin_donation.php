4416528a199dd65d058e3e2dd337b9d55a724244
Adam Lewis <awl@adamwadelewis.com>
2019-03-28 13:50:26 -0500

    Fix missing default in editvoltable div tag

--- admin/admin_donation.php
+++ admin/admin_donation.php
@@ -330,7 +330,7 @@ $javascript .= "
 				<p>Allows Administrators to edit a donation's information.<br>(NOTE: donations that have not been approved by admin review, will not be listed below.)</p>
 			</div>	
 			
-			<div id="editvoltable"></div>
+			<div id="editvoltable" style="height: 650px"></div>
 			
 			<center>
 			<form name="form_search_for_donation" id="form_search_for_donation" method="post" style="margin:25px 10px" 
